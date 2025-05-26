851d6d83aa14dde68d7b8d32630f45c91b06904a
otsukimikitsune <tamakana61@gmail.com>
2025-05-27 01:43:18 +0900

    Remove unnecessary experimental.appDir from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
